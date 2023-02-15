c216cca05446ae4aac8087985da6e451d28c4097
Ketevan Nanuashvili <100858778+KetiNanuashvili@users.noreply.github.com>
2023-02-15 22:27:10 +0400

    add hover div

--- react1/src/Img.js
+++ react1/src/Img.js
@@ -1,14 +1,24 @@
 import './Img.css';
 import iconImg from './images/Shape.svg'
-import { useState } from 'react';
+
 
 function Images(){
+
+    
+    // const [isHovering, setIsHovering]= useState(false);
+
+    // function handleMouseEnter() {
+    //     setIsHovering(true);
+    // }
+
+    // function handleMouseLeave(){
+    //     setIsHovering(false);
+    // }
+
     return (
         <div className="picture-div">
-            {/* <img src={img1} alt="mainimage" className='Main-image'></img> */}
-            <div className="transitionDiv">
-            <img className="iconImg" src={iconImg} alt='iconImg'></img>
-            </div>
+            <div className='transitionDiv'></div>
+            <img className='iconImg'src={iconImg} alt='iconImg' ></img>
         </div>
     )
 }
